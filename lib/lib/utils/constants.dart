b3315af71a38a4812f11a97f9fc59cac369aa3e6
Rohit Kumar <149245736+rohit070805@users.noreply.github.com>
2025-01-30 00:42:27 +0530

    Delete lib/lib directory

--- lib/lib/utils/constants.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-class Constants{
-  List<String> classes = ['Ist','2nd','3rd','4th','5th','6th','7th','8th','9th','11th','12th'];
-  List<String> present = ['20','23','36','24','41','16','27','38','13','31','45'];
-  int teachercount = 4;
-}
