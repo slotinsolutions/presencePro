0538ee846fd27d301d8d97078fed61683d3c268b
Rohit Kumar <149245736+rohit070805@users.noreply.github.com>
2025-02-16 00:00:57 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -2,49 +2,44 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-import 'package:testapp/pages/AdminHomePage.dart';
-import 'package:testapp/pages/LoginScreen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:testapp/pages/SplashScreen.dart';
-import 'package:testapp/pages/selectType.dart';
 import 'package:testapp/utils/theme_provider.dart';
 
-
-
-
-void main()async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  // Load the theme before starting the app
+  ThemeProvider themeProvider = ThemeProvider();
+  await themeProvider.loadTheme(); // Wait for theme to load
+
   runApp(
     ChangeNotifierProvider(
-      create: (context) => ThemeProvider(),
+      create: (context) => themeProvider,
       child: const MyApp(),
     ),
   );
-
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-        theme: ThemeData(
-          primaryColor: themeProvider.themeColor,
-          scaffoldBackgroundColor: themeProvider.isDarkMode ? Colors.grey[900] : Colors.white,
-          textTheme: TextTheme(
-            bodyLarge: TextStyle(color: themeProvider.textColor),
-            bodyMedium: TextStyle(color: themeProvider.textColor),
-          ),
+      theme: ThemeData(
+        primaryColor: themeProvider.themeColor,
+        scaffoldBackgroundColor: themeProvider.isDarkMode ? Colors.grey[900] : Colors.white,
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(color: themeProvider.textColor),
+          bodyMedium: TextStyle(color: themeProvider.textColor),
         ),
+      ),
       home: Splashscreen(),
     );
   }
 }
-
--- lib/utils/theme_provider.dart
+++ lib/utils/theme_provider.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
   bool isDarkMode = false;
-
 
   static const Color lightThemeColor = Colors.white;
   static const Color darkThemeColor = Color(0xFF212121);
@@ -12,8 +12,24 @@ class ThemeProvider extends ChangeNotifier {
   Color get themeColor => isDarkMode ? darkThemeColor : lightThemeColor;
   Color get textColor => isDarkMode ? darkTextColor : lightTextColor;
 
-  void toggleTheme() {
+  ThemeProvider() {
+    loadTheme(); // Load theme when app starts
+  }
+
+  void toggleTheme() async {
     isDarkMode = !isDarkMode;
+    await saveTheme(isDarkMode);
+    notifyListeners();
+  }
+
+  Future<void> saveTheme(bool value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool("isDarkMode", value);
+  }
+
+  Future<void> loadTheme() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    isDarkMode = prefs.getBool("isDarkMode") ?? false;
     notifyListeners();
   }
 }
